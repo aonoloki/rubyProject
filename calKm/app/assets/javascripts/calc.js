678488b8f7236d1d10166cf8ce7ba2d2cfaf5815
aonoloki <r.gorgibus@gmail.com>
2017-04-19 15:46:11 +0200

    Back to 0.1

--- calKm/app/assets/javascripts/calc.js
+++ calKm/app/assets/javascripts/calc.js
@@ -1,16 +1,3 @@
 jQuery(function($) {
-  alert('on!')
-  var hp = $('#path_horsepower').val()
-  var distance = $('#path_distance').val()
-  var total = hp + distance
-  var horse = parseInt(hp)
-  var dist = parseInt(distance)
 
-  $('#new_path').on('submit', function(){
-    $('#path_total').val(total)
-    console.log(horse)
-    console.log(dist)
-    console.log(total)
-    return false
-  })
 })
